dc6d21b47662271adff1e994ccdda5f4317f3690
parunkov <parunkov@gmail.com>
2024-01-25 14:34:41 +0500

    inputs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script setup>
+import { ref } from 'vue';
+
+const login = ref('');
+const password = ref('');
+
+const onLoginInput = (event) => {
+  login.value = event.target.value;
+}
+
+const onPasswordInput = (event) => {
+  password.value = event.target.value;
+}
+
+const auth = async (login, password) => {
+  console.log(login, password);
+}
+
+const onAuthClick = () => {
+  auth(login.value, password.value);
+}
 </script>
 
 <template>
   <div>
-    <input type="text" name="login">
-    <input type="text" name="password">
-    <button type="button">Войти</button>
+    <input type="text" name="login" @input="onLoginInput">
+    <input type="password" name="password" @input="onPasswordInput">
+    <button type="button" @click="onAuthClick">Войти</button>
   </div>
 </template>
 
 <style scoped>
-input, button {
+input,
+button {
   display: block;
   margin-bottom: 10px;
 }
