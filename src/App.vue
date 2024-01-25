9461fd329efd12a7bb2c4c48183b1dd9f6690fb9
parunkov <parunkov@gmail.com>
2024-01-25 14:06:34 +0500

    app created

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <input type="text" name="login">
+    <input type="text" name="password">
+    <button type="button">Войти</button>
+  </div>
+</template>
+
+<style scoped>
+input, button {
+  display: block;
+  margin-bottom: 10px;
+}
+</style>
